472b60275523f659e99e0592ac53dcc627f27f70
Steve Waldman <swaldman@mchange.com>
2025-08-10 16:44:47 -0400

    Test git-rather-than-rsync-based republish.sh

--- republish.sh
+++ republish.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-./tech-site-gen
-git add .
-git commit -m "$1"
-git push
-pushd public
-rsync -av . swaldman@tickle.mchange.com:/home/web/public/tech
-popd
+./tech-site-gen       &&
+git add .             &&
+git commit -m "$1"    &&
+git push              &&
+./tickle-pull-tech.sh
 
---
+++ tickle-pull-tech.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ssh tickle 'cd /home/web/public/tech.interfluidity.com/; git pull'
+
