a2cf00462f482b8afacdb76d2493a473799c1896
Steve Waldman <swaldman@mchange.com>
2025-08-10 16:46:44 -0400

    Test git-rather-than-rsync-based republish.sh

--- tickle-pull-tech.sh
+++ tickle-pull-tech.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh tickle6.mchange.com 'cd /home/web/public/tech.interfluidity.com/ && git pull'
+ssh -A tickle6.mchange.com 'cd /home/web/public/tech.interfluidity.com/ && git pull'
 
